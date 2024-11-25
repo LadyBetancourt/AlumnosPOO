72a8fe8a1c747240de9562e6f2c37729f978f5fa
LadyBetancourt <lady_betancourt24@unikino.edu.mx>
2024-11-25 08:01:54 -0700

    Agregar archivos de proyecto.

---
+++ AlumnosPOO/Alumno.cs
@@ -0,0 +1,44 @@
+﻿namespace AlumnosPOO
+{
+    internal class Alumno
+    {
+        string nombre;
+        string grupo;
+        string contrasena;
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Grupo
+        {
+            get
+            {
+                return grupo;
+            }
+            set
+            {
+                grupo = value;
+            }
+        }
+        public string Contrasena
+        {
+            get
+            {
+                return contrasena;
+            }
+            set
+            {
+                contrasena = value;
+            }
+        }
+    }
+}
---
+++ AlumnosPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AlumnosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno mora = new Alumno();
+            mora.Nombre = "Santiago Mora";
+            mora.Grupo = "DGDIE11";
+            mora.Contrasena = "abc123";
+
+            Console.WriteLine(mora.Contrasena);
+        }
+    }
+}
